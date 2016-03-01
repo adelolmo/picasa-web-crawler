7cb3838d3ddacf4afed3a6f689c666a7089e9793
adelolmo <andoni.delolmo@digitalperformance.de>
2016-03-01 17:22:44 +0100

    allow donwload single album.

--- src/main/java/org/ado/picasa/Main.java
+++ src/main/java/org/ado/picasa/Main.java
@@ -1,13 +1,5 @@
 package org.ado.picasa;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
@@ -16,6 +8,18 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 /**
  * @author Andoni del Olmo
@@ -26,6 +30,13 @@ public class Main {
     private static final String TMP_DIR = "/tmp/picasa";
 
     public static void main(String[] args) throws Exception {
+        final Options options = new Options();
+        options.addOption("a", true, "Album name");
+        options.addOption("v", true, "Verification code");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+//        System.exit(1);
         validateEnvironmentVariables();
         FileUtils.deleteQuietly(new File(TMP_DIR));
         FileUtils.forceMkdir(new File(TMP_DIR));
@@ -36,28 +47,49 @@ public class Main {
         profile.setPreference("browser.download.dir", TMP_DIR);
         profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/jpeg,image/png");
         final FirefoxDriver driver = new FirefoxDriver(profile);
-        loginIntoPicasa(args[0], driver);
+        loginIntoPicasa(cmd.getOptionValue("v"), driver);
 
         driver.navigate().to("https://picasaweb.google.com/home?showall=true");
         TimeUnit.SECONDS.sleep(2);
 
-//        driver.findElementByLinkText("Anja").click();
-        // <a class="gphoto-album-cover-link"
-        final Set<String> albumHrefs =
-                driver.findElements(By.xpath("//div[@id='lhid_albums']//a[@class='gphoto-album-cover-link']")).stream()
-                        .map(a -> a.getAttribute("href"))
-                        .collect(Collectors.toSet());
-        TimeUnit.SECONDS.sleep(1);
+        final List<WebElement> albumLinks =
+            driver.findElements(By.xpath("//p[@class='gphoto-album-cover-title']/a"));
 
-        for (String album : albumHrefs) {
+        if (cmd.hasOption("a")) {
+            final String albumName = cmd.getOptionValue("a");
+            System.out.println(albumName);
+            downloadAlbum(driver,
+                albumLinks.stream()
+                    .filter(al -> al.getText().equals(albumName))
+                    .findFirst().get()
+                    .getAttribute("href"));
+
+        } else {
+
+            final Set<String> albumHrefs =
+                albumLinks.stream()
+                    .map(a -> a.getAttribute("href"))
+                    .collect(Collectors.toSet());
+
+            albumHrefs.forEach(a -> downloadAlbum(driver, a));
+
+        }
+        System.out.println("done");
+        System.out.println("end: " + new Date().toString());
+//        TimeUnit.SECONDS.sleep(60);
+//        driver.close();
+    }
+
+    private static void downloadAlbum(FirefoxDriver driver, String album) {
+        try {
             System.out.println("> " + album);
             final String albumName = getAlbumName(album);
             System.out.println(albumName);
             driver.navigate().to(album);
 
             final List<String> hrefs = driver.findElements(By.xpath("//div[@class='goog-icon-list']//a"))
-                    .stream().map(a -> a.getAttribute("href"))
-                    .collect(Collectors.toList());
+                .stream().map(a -> a.getAttribute("href"))
+                .collect(Collectors.toList());
 
             for (String href : hrefs) {
                 downloadPhoto(driver, href);
@@ -67,9 +99,9 @@ public class Main {
             System.out.println("moving photos to directory: " + albumName);
             final File albumDirectory = new File(TMP_DIR, albumName);
             final Collection<File> files =
-                    FileUtils.listFiles(new File(TMP_DIR),
-                            TrueFileFilter.INSTANCE,
-                            TrueFileFilter.INSTANCE);
+                FileUtils.listFiles(new File(TMP_DIR),
+                    TrueFileFilter.INSTANCE,
+                    TrueFileFilter.INSTANCE);
             for (File file : files) {
                 try {
                     FileUtils.moveFileToDirectory(file, albumDirectory, true);
@@ -77,12 +109,10 @@ public class Main {
                     FileUtils.moveFile(file, new File(albumDirectory, file.getName() + "-" + System.currentTimeMillis()));
                 }
             }
+        } catch (Exception e) {
+            System.out.println("Error! Cannot download album '" + album + "'");
+            e.printStackTrace();
         }
-
-        System.out.println("done");
-        System.out.println("end: " + new Date().toString());
-        TimeUnit.SECONDS.sleep(60);
-        driver.close();
     }
 
     private static String getAlbumName(String album) {
@@ -111,7 +141,7 @@ public class Main {
 
     private static void validateEnvironmentVariables() {
         if (StringUtils.isEmpty(System.getenv("GOOGLE_ACCOUNT"))
-                || StringUtils.isEmpty(System.getenv("GOOGLE_PASSWORD"))) {
+            || StringUtils.isEmpty(System.getenv("GOOGLE_PASSWORD"))) {
             System.out.println("Missing environment variables GOOGLE_ACCOUNT or GOOGLE_PASSWORD");
             System.exit(1);
         }
@@ -125,8 +155,10 @@ public class Main {
         driver.findElement(new By.ByName("Passwd")).sendKeys(System.getenv("GOOGLE_PASSWORD"), Keys.ENTER);
         TimeUnit.SECONDS.sleep(1);
 
-        driver.findElement(new By.ByName("Pin")).sendKeys(pin, Keys.ENTER);
-        TimeUnit.SECONDS.sleep(1);
+        if (StringUtils.isNotBlank(pin)) {
+            driver.findElement(new By.ByName("Pin")).sendKeys(pin, Keys.ENTER);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
 }
